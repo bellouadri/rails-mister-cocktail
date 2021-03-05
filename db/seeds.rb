82d96b4dbfe63aaf43aa574b309014c833edb13c
Adrien Bellucci <adrien.bellucci60@gmail.com>
2021-03-05 11:15:40 +0100

    done with day one of cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+parsed_json_ingredients = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+
+parsed_json_ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+
+
+Cocktail.create(name: 'Mojito')
